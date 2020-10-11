00a4b6f2c19d9fe2562be48daee3a70b8a2f8408
Haggle1996 <haggle1996@gmail.com>
2020-10-11 00:09:29 -0600

    Added rules for mob spanwning

--- src/main/java/haggle1996/haggletweaks/HaggleTweaks.java
+++ src/main/java/haggle1996/haggletweaks/HaggleTweaks.java
@@ -7,6 +7,13 @@ import net.minecraft.util.ChatComponentTranslation;
 import net.minecraft.util.ChunkCoordinates;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingSpawnEvent;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityList;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.monster.IMob;
+import net.minecraft.world.EnumSkyBlock;
+import net.minecraft.world.World;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -20,24 +27,25 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.ItemStackHolderInjector;
 import cpw.mods.fml.common.event.FMLLoadCompleteEvent;
 import cpw.mods.fml.common.eventhandler.Event.Result;
+import cpw.mods.fml.common.eventhandler.EventPriority;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
-@Mod(modid = NoSleeping.MODID, version = NoSleeping.VERSION, name = NoSleeping.NAME,  dependencies = "required-after:Forge@[10.13.3.1384,11.14);after:*;")
-public class NoSleeping {
+@Mod(modid = HaggleTweaks.MODID, version = HaggleTweaks.VERSION, name = HaggleTweaks.NAME,  dependencies = "required-after:Forge@[10.13.3.1384,11.14);after:*;")
+public class HaggleTweaks {
     public static final String MODID = "HaggleTweaks";
     public static final String NAME = "HaggleTweaks";
-    public static final String VERSION = "0.2.0.0";
+    public static final String VERSION = "0.3.0.0";
     public static final Logger log = LogManager.getLogger(MODID);
     
  	@Mod.EventHandler
 	public void preInit (FMLPreInitializationEvent event)
 	{
 		
-        NoSleeping.log.info("HaggleTweaking Started...");
+        HaggleTweaks.log.info("HaggleTweaking Started...");
 
         MinecraftForge.EVENT_BUS.register(this);
 
-        NoSleeping.log.info("Finished HaggleTweaking");
+        HaggleTweaks.log.info("Finished HaggleTweaking");
 	}
 
     @SubscribeEvent
@@ -52,5 +60,20 @@ public class NoSleeping {
         }
 
     }
-    
+
+    @SubscribeEvent(priority = EventPriority.HIGH)
+    public void CheckSpawn(LivingSpawnEvent.CheckSpawn event)
+    {
+        if (event.entity instanceof IMob)
+        {
+            if (event.world.provider.dimensionId == 0)
+            {
+                if (!event.world.isRaining())
+                {
+                    event.setResult(Result.DENY);
+                }
+            }
+        }
+    }
+
 }
